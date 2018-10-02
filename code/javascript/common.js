94bbcbdc550bd63c2e5f27e895122ff94f69c969
薛健超 <2289565671@qq.com>
2018-10-02 23:52:02 +0800

    some

---
+++ code/javascript/common.js
@@ -0,0 +1,16 @@
+
+// 用来在浏览器窗口宽度发生变化时,改变根元素的font-size
+// 使之保持font-size = 100*(变化后的宽度/变化前的宽度)
+(function(doc,win){
+  var docEI = doc.documentElement,
+  resizeEvt = 'orientationchange' in window?'orientataionchange':'resize',
+  recalc = function(){
+      var clientWidth = docEI.clientWidth;
+      if(!clientWidth) return;
+      docEI.style.fontSize = 100*(clientWidth/375)+'px';
+  }
+
+  if(!doc.addEventListener) return;
+  win.addEventListener(resizeEvt, recalc, false);
+  doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document,window);
