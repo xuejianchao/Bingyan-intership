f94701295ce8f51498787cbd99209fbbc4aadd99
薛健超 <2289565671@qq.com>
2018-10-04 10:24:39 +0800

    the points in the onboard page

--- code/javascript/pic-swipe.js
+++ code/javascript/pic-swipe.js
@@ -1,3 +1,5 @@
+var points = document.querySelector('.points').children;
+
 var PicSlide = function (el, swipe, options) {
   this.options = options || {}; //可选函数
   this.current = 0; //当前页面索引
@@ -10,6 +12,8 @@ var PicSlide = function (el, swipe, options) {
   this.pics = el; //当前页面的对象
   this.allPics = el.querySelector('li');
   this.swipe = swipe || 'X'; //滑动方向参数
+  this.isnextnextinscreen = false;
+  this.isprevprevinscreen = false;
   this.resize().init().bindEvents();
 };
 
@@ -23,12 +27,14 @@ PicSlide.prototype.init = function () {
   var current = this.pics.firstElementChild;
   var pics = this.pics.children;
   var total = pics.length;
-  console.log(pics);
-  console.log(pics[3]);
 
   this.setX(current, 0.2, 'rem');
   this.setX(pics[total - 1], -3.25, 'rem');
   this.setX(pics[1], 3.65, 'rem');
+
+  //初始化点列
+  points[this.current].classList.remove('gray');
+  points[this.current].classList.add('darkgray');
 
   for (var i = 2; i < total - 1; i++) {
     this.setX(pics[i], 3.76, 'rem');
@@ -57,6 +63,8 @@ PicSlide.prototype.touchstart = function (ev) {
   //记录落点
   this.pageX = touches.pageX;
   this.pageY = touches.pageY;
+
+  clearTimeout(picRollTimer);
 };
 
 PicSlide.prototype.touchmove = function (ev) {
@@ -89,16 +97,18 @@ PicSlide.prototype.touchmove = function (ev) {
     this.setX(prev, -3.25 + X / 100, 'rem');
 
     if (X < -335) {
-      //左滑,要显示出next 的next
+      //左滑,要显示出nextnext
       var nextnext = this.getNextNext();
       if (nextnext) {
         this.setX(nextnext, 7.2 + X / 100, 'rem');
+        this.isnextnextinscreen = true;
       }
     } else if (X > 335) {
       //右滑,显示出prev的prev
       var prevprev = this.getPrevPrev();
       if (prevprev) {
         this.setX(prevprev, X / 100 - 6.7, 'rem');
+        this.isprevprevinscreen = true;
       }
     }
   }
@@ -173,6 +183,8 @@ PicSlide.prototype.backtoPlace = function () {
   //注意,看起来应该是任务被放进事件处理队列之后几乎立刻就被执行了
   //所以settimeout的时间应该和动画的时间一样
   //多出来的时间好像还能造成一种缓入的效果
+
+  picRollTimer = setTimeout(self.next.bind(self), timePeriod);
 }
 
 PicSlide.prototype.next = function () {
@@ -185,13 +197,22 @@ PicSlide.prototype.next = function () {
 
   curr.classList.add('toLeft');
   next.classList.add('toCenter');
-  this.setX(nextnext, 4.15, 'rem'); 
+  if (!this.isnextnextinscreen) {
+    this.setX(nextnext, 4.15, 'rem');
+    this.isnextnextinscreen = false;
+  }
   nextnext.classList.add('toRight');
-  this.setX(prev, 3.75, 'rem');
+  this.setX(prev, 3.76, 'rem');
+
+  this.changeToGray(this.current);
 
   this.current++;
   var total = this.pics.children.length;
   this.current = this.current % total;
+
+  this.changeToDarkgray(this.current);
+
+
 
   setTimeout(function () {
     curr.classList.remove('toLeft');
@@ -201,6 +222,8 @@ PicSlide.prototype.next = function () {
     self.setX(nextnext, 3.65, 'rem');
     self.setX(next, 0.2, 'rem');
   }, 500);
+
+  picRollTimer = setTimeout(self.next.bind(self), timePeriod);
 }
 
 PicSlide.prototype.prev = function () {
@@ -212,17 +235,22 @@ PicSlide.prototype.prev = function () {
 
   curr.classList.add('toRight');
   prev.classList.add('toCenter');
-  this.setX(prevprev, -3.75, 'rem'); 
+  if (!this.isprevprevinscreen) {
+    this.setX(prevprev, -3.75, 'rem');
+    this.isprevprevinscreen = false;
+  }
   //↑先把图片移动到屏幕左侧外面,这样从右侧移动到左侧的过程就不算在动画中了
   //而且因为移动之后,图片右侧离屏幕左侧还有一点距离,所以prevprev和prev刚好能同时停止动作
   //太投机取巧了,而且不同尺寸不好复用
   prevprev.classList.add('toLeft');
   this.setX(next, 3.75, 'rem');
 
+  this.changeToGray(this.current);
   this.current--;
-  if(this.current< 0){
-    this.current = this.pics.children.length-1;
+  if (this.current < 0) {
+    this.current = this.pics.children.length - 1;
   }
+  this.changeToDarkgray(this.current);
 
   setTimeout(function () {
     curr.classList.remove('toRight');
@@ -233,8 +261,9 @@ PicSlide.prototype.prev = function () {
     self.setX(prevprev, -3.25, 'rem');
     //上
   }, 500);
-
+  picRollTimer = setTimeout(self.next.bind(self), timePeriod);
 }
+
 PicSlide.prototype.setX = function (el, x, unit) {
   el && (el.style.webkitTransform = 'translate3d(' + x + (unit || 'px') + ',0,0)');
 };
@@ -256,4 +285,19 @@ PicSlide.prototype.getPrev = function () {
   }
 }
 
+//用来改变点的颜色的↓
+PicSlide.prototype.changeToGray = function (i) {
+  points[i].classList.remove('darkgray');
+  points[i].classList.add('gray');
+}
+
+PicSlide.prototype.changeToDarkgray = function (i) {
+  points[i].classList.remove('gray');
+  points[i].classList.add('darkgray');
+}
+
 var piclist = new PicSlide(document.querySelector('.pics'), 'X');
+
+//轮播的设置↓
+var timePeriod = 3000;
+var picRollTimer = setTimeout(piclist.next.bind(piclist), timePeriod);
