d98a48920c0fac2a36223879642254b4166f65e8
薛健超 <2289565671@qq.com>
2018-10-06 17:10:52 +0800

    ajax and backend

--- code/app.js
+++ code/app.js
@@ -1,5 +1,6 @@
 var http = require("http");
 var fs = require('fs');
+var url = require('url');
 var Router = require('node-router');
 
 var routers = new Router();
@@ -19,33 +20,132 @@ var mime = {
 var foodinfo = {
   type: 'foodcontent',
   content: [{
-    //还要考虑怎么给每个食物生成一个界面==>用ajax要来的数据填充detail上的大部分内容
-    name: 'Veggie Cheese Extravaganza',
-    pic: '../images/th(2).png', //目前都只用这一个
-    point: '2',
-    descript: "For a vegetarian looking for a BIG treat that goes easy on the spices, this one's got it all. The onions, the capsicum, those delectable mushrooms - with paneer and golden corn to top it all.",
-    basicPrice: 199,
-    selection: {
-      size: [
-        ['Medium', 251, '₹450'],
-        ['Large', 791, '₹990'],
-        ['Small', 0, '₹199']
-      ],
-      crust: [
-        ['Stanard', 0, ''],
-        ['Garlic Roasted', 0, 'free'],
-        ['Cheese Burst', 0, 'free']
-      ],
-      Topping: [
-        ['Stanard', 0, ''],
-        ['Extra Cheese', 0, ''],
-        ['Extra Spice', 0, '']
-      ]
+      //还要考虑怎么给每个食物生成一个界面==>用ajax要来的数据填充detail上的大部分内容
+      id: '1',
+      name: 'Veggie Cheese Extravaganza',
+      pic: '../images/th(2).png', //目前都只用这一个
+      point: '2',
+      descript: "For a vegetarian looking for a BIG treat that goes easy on the spices, this one's got it all. The onions, the capsicum, those delectable mushrooms - with paneer and golden corn to top it all.",
+      basicPrice: 199,
+      selection: {
+        Size: [
+          ['Medium', 251, '₹450'],
+          ['Large', 791, '₹990'],
+          ['Small', 0, '₹199']
+        ],
+        Crust: [
+          ['Stanard', 0, ''],
+          ['Garlic Roasted', 0, 'free'],
+          ['Cheese Burst', 0, 'free']
+        ],
+        Topping: [
+          ['Stanard', 0, ''],
+          ['Extra Cheese', 0, '₹99'],
+          ['Extra Spice', 0, '']
+        ]
+      }
+    }, {
+      id: "2",
+      name: 'Paneer Pan Pizza',
+      pic: '../images/pannerPizza.jpg',
+      point: '5',
+      descript: "Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor. Pellentesque non dignissim neque. ",
+      basicPrice: 199,
+      selection: {
+        Size: [
+          ['Medium', 251, '₹450'],
+          ['Large', 791, '₹990'],
+          ['Small', 0, '₹199']
+        ],
+        Crust: [
+          ['Stanard', 0, ''],
+          ['Garlic Roasted', 0, 'free'],
+          ['Cheese Burst', 0, 'free']
+        ],
+        Topping: [
+          ['Stanard', 0, ''],
+          ['Extra Cheese', 99, '₹99'],
+          ['Extra Spice', 0, '']
+        ]
+      }
+    }, {
+      id: "3",
+      name: 'Burger',
+      pic: '../images/burger.jpg',
+      point: '4',
+      descript: "Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor. Pellentesque non dignissim neque. ",
+      basicPrice: 149,
+      selection: {
+        Size: [
+          ['Medium', 50, '₹199'],
+          ['Large', 150, '₹299'],
+          ['Small', 0, '₹149']
+        ],
+        Stuffing: [
+          ['Stanard', 0, ''],
+          ['Extra Cheese', 49, '₹49'],
+          ['Extra Meet', 0, '']
+        ]
+      }
+    }, {
+      id: "4",
+      name: 'Salad',
+      pic: '../images/salad.jpg',
+      point: '3',
+      descript: "Donec facilisis tortor ut augue lacinia, at viverra est semper. Sed sapien metus, scelerisque nec pharetra id, tempor a tortor. Pellentesque non dignissim neque. ",
+      basicPrice: 149,
+      selection: {
+        Size: [
+          ['Medium', 50, '₹199'],
+          ['Large', 150, '₹299'],
+          ['Small', 0, '₹149']
+        ],
+        Stuffing: [
+          ['Stanard', 0, ''],
+          ['Extra Cheese', 49, '₹49'],
+          ['Extra Meet', 0, '']
+        ]
+      }
     }
-  }, {
 
-  }]
+
+  ]
 }
+
+
+router(function (req, res, next) {
+  var urlParsed = url.parse(req.url);
+  console.log(urlParsed);
+
+  if (urlParsed.pathname === '/foodDetailPage') {
+    //这个就只是用来接收"前往food-detail"的请求的.
+    fs.readFile('./html/foodDetailPage.html', function (err, data) {
+      if (err) {
+        res.send(err);
+      } else {
+        res.writeHead('200', {
+          "Conten-Tpye": 'text/html'
+        });
+        res.write(data);
+        res.end();
+      }
+    })
+  } else if (urlParsed.pathname === '/foodDetailContent') {
+    //用来响应"请给我某个食物的详细信息"的请求.
+    var targetid = urlParsed.query.split('=')[1];
+
+    //根据querystring中的id找到目标食物,然后返回
+    //这里不能用forEach();
+    for(var i=0;i<foodinfo.content.length;i++){
+      if(targetid == foodinfo.content[i].id){
+        res.send(JSON.stringify(foodinfo.content[i]));
+      }
+    }
+  } else {
+    next();
+  }
+})
+
 
 //首先实现加载home和menu吧,都是主要是菜单栏
 //↓ 在菜单界面中,响应"给我食物的数据"的请求的路由
@@ -62,7 +162,8 @@ router('/menu/getmenu', function (req, res) {
     var oneFoodInfo = {
       name: foodinfo.content[i].name,
       basicPrice: foodinfo.content[i].basicPrice,
-      pic: foodinfo.content[i].pic
+      pic: foodinfo.content[i].pic,
+      id: foodinfo.content[i].id
     };
     jsonToReturn.content.push(oneFoodInfo);
     console.log(i);
@@ -72,7 +173,7 @@ router('/menu/getmenu', function (req, res) {
 })
 
 //提供静态资源的路由
-router('/menu', '/home', function (req, res) {
+router('/menu', '/home', '/food_detail', function (req, res) {
   if (req.url.split('.')[1] == 'html') {
     var path = './html' + req.url;
   } else {
---
+++ code/javascript/getfoodItems.js
@@ -0,0 +1,33 @@
+//然后放到界面上
+function generAllMoudle(json,selector) {
+  var foodItemArray = json.content;
+  var foodNum = foodItemArray.length;
+  for (var i = 0; i < foodNum; i++) {
+    generOneFood(foodItemArray[i],selector);
+  }
+}
+
+
+//测试没有问题
+function generOneFood(onefoodjson,selector) {
+  var appendSection = document.createElement('section');
+  appendSection.classList.add('foodItem');
+
+  var imgString =
+    `<div class="imgContainer">
+      <a href="/foodDetailPage?id=${onefoodjson.id}"><img class="imgWithRatio" src="${onefoodjson.pic}" alt=""></a>
+      </div>`
+    .trim();
+
+  var infoString =
+    `<div class="foodTextInfo">
+     <h4>${onefoodjson.name}</h4>
+     <h5 class="gray">
+       <span>Starts from </span><span>${onefoodjson.basicPrice}</span>
+     </h5>
+   </div>`
+
+  appendSection.innerHTML = imgString + infoString;
+
+  document.querySelector(selector).append(appendSection);
+}
